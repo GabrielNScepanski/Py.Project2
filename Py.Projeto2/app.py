109e7d33119605999f12d71d3bf9ff390c758884
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-04-01 12:11:23 -0300

    Add files via upload

---
+++ Py.Projeto2/app.py
@@ -0,0 +1,16 @@
+print("1 - Cadastrar restaurante ")
+print("2 - Listar Restaurante")
+print("3 - Atiar Restaurante")
+print("4 - Sair\n")
+
+opcao_digitada = int(input("escolha o numero de 1 a 4"))
+
+if opcao_digitada ==1:
+        print("Você escolheu 1 - Cadastrar Restaurante\n")
+elif opcao_digitada ==2:
+        print("Você escolheu 2 - Listar Restaurante\n")
+elif opcao_digitada ==3:
+        print("Você escolheu 3 - Atiar Restaurante\n")
+elif opcao_digitada ==4:
+         print("Você escolheu 4 -  Sair\n")
+ 
